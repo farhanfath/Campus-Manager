9b9996c754b5e60d7c943cacf7ea1662cabd550b
farhanfath <farhanfathur73@gmail.com>
2024-09-04 18:39:30 +0700

    feat: add all activity layout

--- app/src/main/java/com/project/user/view/home/data/CrudDataActivity.kt
+++ app/src/main/java/com/project/user/view/home/data/CrudDataActivity.kt
@@ -1,4 +1,4 @@
-package com.project.user.view.home
+package com.project.user.view.home.data
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.project.user.R
 
-class DataActivity : AppCompatActivity() {
+class CrudDataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_data)
+        setContentView(R.layout.activity_crud_data)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
